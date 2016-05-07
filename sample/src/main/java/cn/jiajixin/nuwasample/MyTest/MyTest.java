9b5deec1c79200705691d2e04a38aec0a526b7d6
梁鹏飞 <871999566@qq.com>
2016-05-07 10:19:05 +0800

    添加热修复的文件

---
+++ sample/src/main/java/cn/jiajixin/nuwasample/MyTest/MyTest.java
@@ -0,0 +1,11 @@
+package cn.jiajixin.nuwasample.MyTest;
+
+/**
+ * Email : luckyliangfei@gmail.com
+ * Created by fei on 16/5/7.
+ */
+public class MyTest {
+    public static String say() {
+        return "这个是经过热修复过的";
+    }
+}
